7e9dbbe782c78aa9d731f26594d1c8a4a709399a
CesarHernandezPorras12 <cesar.hernandez21153@ucaldas.edu.co>
2023-05-08 21:14:26 -0500

    Ajuste de nueva estructura

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.calculadora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
